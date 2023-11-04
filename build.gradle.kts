3280f2057891296b643b8ca06ca3e260b411c61d
NathanFallet <contact@nathanfallet.me>
2023-11-04 17:22:14 +0100

    Unit model extension

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "me.nathanfallet.usecases"
-version = "1.1.0"
+version = "1.1.1"
 
 repositories {
     mavenCentral()
---
+++ src/commonMain/kotlin/me/nathanfallet/usecases/models/UnitModel.kt
@@ -0,0 +1,7 @@
+package me.nathanfallet.usecases.models
+
+object UnitModel : IModel<Unit, Unit, Unit> {
+
+    override val id = Unit
+
+}
