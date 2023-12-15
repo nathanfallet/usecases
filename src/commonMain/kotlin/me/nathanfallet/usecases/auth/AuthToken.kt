64c19b6e39a88a0a0ab73c94694ee876546df1c2
NathanFallet <contact@nathanfallet.me>
2023-12-15 09:12:38 +0100

    Bumping version

--- src/commonMain/kotlin/me/nathanfallet/usecases/auth/AuthToken.kt
+++ src/commonMain/kotlin/me/nathanfallet/usecases/auth/AuthToken.kt
@@ -8,7 +8,7 @@ data class AuthToken(
     @Schema(name = "Access Token", example = "1234abcd")
     val accessToken: String,
     @Schema(name = "Refresh Token", example = "1234abcd")
-    val refreshToken: String,
+    val refreshToken: String? = null,
     @Schema(name = "ID Token", example = "1234abcd")
     val idToken: String? = null,
 )
